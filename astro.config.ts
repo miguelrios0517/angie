95dcb142d854f56e700541d3cb10cb7cffb4be2a
Miguel Rios <miguel.rios1997@gmail.com>
2026-08-13 10:44:28 -0700

    changed service to psicoterapia

--- astro.config.ts
+++ astro.config.ts
@@ -107,5 +107,6 @@ export default defineConfig({
 
   adapter: cloudflare({
     imageService: 'passthrough', // 👈 Forces Cloudflare to respect the passthrough mode
+    sessionKVBindingName: 'angie-session',
   }),
 });
